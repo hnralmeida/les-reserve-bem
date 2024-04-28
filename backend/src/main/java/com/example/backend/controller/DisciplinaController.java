9a2e99edd3cce210811ab7751092f27a0b52eccb
matheusSpelta <matheuszacche@hotmail.com>
2024-04-28 12:30:53 -0300

    Rotas Disciplina criadas

--- backend/src/main/java/com/example/backend/controller/DisciplinaController.java
+++ backend/src/main/java/com/example/backend/controller/DisciplinaController.java
@@ -1,6 +1,11 @@
 package com.example.backend.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +25,20 @@ public class DisciplinaController {
     @PostMapping("/cadastro")
     public Disciplina cadastrarDisciplina(@RequestBody Disciplina disciplina, @RequestBody Coordenadoria coordenadoria){
         return disciplinaService.cadastrarDisciplina(disciplina, coordenadoria);
+    }
+
+    @GetMapping("/listagem")
+    public List<Disciplina> listarDisciplinas(){
+        return disciplinaService.listarDisciplinas();
+    }
+
+    @GetMapping("/{id}")
+    public Disciplina buscarDisciplinaPorId(@PathVariable Long id, @RequestBody Disciplina disciplina){
+        return disciplinaService.editarDisciplina(id, disciplina);
+    }
+
+    @DeleteMapping("/{id}")
+    public void excluirDisciplina(@PathVariable Long id){
+        disciplinaService.excluirDisciplina(id);
     }
 }
