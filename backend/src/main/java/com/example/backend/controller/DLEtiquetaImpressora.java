4a39a97d16d4b70c4ede0c5d6ef791994872a689
Matheusszs <matheuszacche@hotmail.com>
2024-07-01 09:45:59 -0300

    Criação das rotas de impressão para etiquetas

---
+++ backend/src/main/java/com/example/backend/controller/DLEtiquetaImpressora.java
@@ -0,0 +1,35 @@
+package com.example.backend.controller;
+
+import java.io.IOException;
+
+import javax.print.PrintException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.backend.dominio.DTOPrinter.EtiquetaPrinter;
+import com.example.backend.service.DLEtiquetaService;
+
+import net.sourceforge.barbecue.BarcodeException;
+import net.sourceforge.barbecue.output.OutputException;
+
+@RestController
+@RequestMapping("/etiquetas")
+public class DLEtiquetaImpressora {
+
+    @Autowired
+    private DLEtiquetaService dlEtiquetaService;
+
+    @PostMapping
+    public void print(@RequestBody EtiquetaPrinter request) {
+        try {
+            dlEtiquetaService.generateBarCode(request.getMatricula());
+        } catch (OutputException | IOException | BarcodeException | PrintException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ backend/src/main/java/com/example/backend/dominio/DTOPrinter/EtiquetaPrinter.java
@@ -0,0 +1,12 @@
+package com.example.backend.dominio.DTOPrinter;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EtiquetaPrinter {
+    private String matricula;
+    private String nome;
+}
