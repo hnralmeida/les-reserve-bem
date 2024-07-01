83c7e3a5916ba4301447120ae29b5991421afeef
Matheusszs <matheuszacche@hotmail.com>
2024-07-01 01:41:21 -0300

    Rotas de Impressão para horario de professor e aluno prontas

--- backend/src/main/java/com/example/backend/controller/EspsonTermalController.java
+++ backend/src/main/java/com/example/backend/controller/EspsonTermalController.java
@@ -1,6 +1,7 @@
 package com.example.backend.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.backend.dominio.TermicalPrint;
+import com.example.backend.dominio.DTOPrinter.AulaPrinter;
 import com.example.backend.service.EpsonTermicaService;
 
 import org.springframework.web.bind.annotation.*;
@@ -19,12 +21,30 @@ public class EspsonTermalController {
     @Autowired
     private EpsonTermicaService epsonTermicaService;
 
-    @PostMapping
+    @PostMapping("/test")
     public void print(@RequestBody TermicalPrint request) {
         System.out.println(request.getPrinterName());
         System.out.println(request.getTexto());
         try {
-            epsonTermicaService.print(request.getPrinterName(), request.getTexto());
+            epsonTermicaService.print(request.getTexto());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @PostMapping("/horariosAluno")
+    public void printAulasAluno(@RequestBody List<AulaPrinter> aulas) {
+        try {
+            epsonTermicaService.printAulasAluno(aulas);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @PostMapping("/horariosProfessor")
+    public void printAulasProfessor(@RequestBody List<AulaPrinter> aulas) {
+        try {
+            epsonTermicaService.printAulasProfessor(aulas);
         } catch (IOException e) {
             e.printStackTrace();
         }
--- backend/src/main/java/com/example/backend/service/EpsonTermicaService.java
+++ backend/src/main/java/com/example/backend/service/EpsonTermicaService.java
@@ -5,24 +5,24 @@ import java.util.List;
 
 import javax.print.PrintService;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.example.backend.dominio.Aula;
+import com.example.backend.dominio.DTOPrinter.AulaPrinter;
 import com.github.anastaciocintra.escpos.EscPos;
 import com.github.anastaciocintra.output.PrinterOutputStream;
 
 @Service
 public class EpsonTermicaService {
 
-    public void print(String printerName, String texto) throws IOException {
-        if (printerName == null || printerName.isEmpty()) {
-            System.out.println("Printer name cannot be null or empty");
-            return;
-        }
+    @Value("${printer.name}")
+    private String printerName;
 
+    public void print(String texto) throws IOException {
         PrintService printService = PrinterOutputStream.getPrintServiceByName(printerName);
         if (printService == null) {
-            System.out.println("Print service not found: " + printerName);
+            System.out.println("Impressora não localizada");
             return;
         }
 
@@ -33,6 +33,57 @@ public class EpsonTermicaService {
                 escpos.writeLF(texto);
             } else {
                 System.out.println("Text to print is null");
+            }
+
+            escpos.feed(5).cut(EscPos.CutMode.FULL);
+        }
+    }
+
+    public void printAulasAluno(List<AulaPrinter> aulas) throws IOException {
+        PrintService printService = PrinterOutputStream.getPrintServiceByName(printerName);
+        if (printService == null) {
+            System.out.println("Impressora não localizada");
+            return;
+        }
+        try (PrinterOutputStream printerOutputStream = new PrinterOutputStream(printService);
+                EscPos escpos = new EscPos(printerOutputStream)) {
+
+            if (!aulas.isEmpty()) {
+                AulaPrinter firstAula = aulas.get(0);
+                escpos.writeLF("Aluno: " + firstAula.getAlunoNome());
+                escpos.writeLF("");
+            }
+            for (AulaPrinter aula : aulas) {
+                escpos.writeLF(aula.getDiaDaSemana() + " : " + aula.getHoraInicio() + " - " + aula.getHoraFim());
+                escpos.writeLF("Disciplina: " + aula.getDisciplinaNome());
+                escpos.writeLF("Professor: " + aula.getProfessorNome());
+                escpos.writeLF("Local: " + aula.getLocalNome());
+                escpos.feed(1);
+            }
+
+            escpos.feed(5).cut(EscPos.CutMode.FULL);
+        }
+    }
+
+    public void printAulasProfessor(List<AulaPrinter> aulas) throws IOException {
+        PrintService printService = PrinterOutputStream.getPrintServiceByName(printerName);
+        if (printService == null) {
+            System.out.println("Impressora não localizada");
+            return;
+        }
+        try (PrinterOutputStream printerOutputStream = new PrinterOutputStream(printService);
+                EscPos escpos = new EscPos(printerOutputStream)) {
+
+            if (!aulas.isEmpty()) {
+                AulaPrinter firstAula = aulas.get(0);
+                escpos.writeLF("Professor: " + firstAula.getProfessorNome());
+                escpos.writeLF("");
+            }
+            for (AulaPrinter aula : aulas) {
+                escpos.writeLF(aula.getDiaDaSemana() + " : " + aula.getHoraInicio() + " - " + aula.getHoraFim());
+                escpos.writeLF("Disciplina: " + aula.getDisciplinaNome());
+                escpos.writeLF("Local: " + aula.getLocalNome());
+                escpos.feed(1);
             }
 
             escpos.feed(5).cut(EscPos.CutMode.FULL);
